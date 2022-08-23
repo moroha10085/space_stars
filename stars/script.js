11c05eea37469d0783311e96b83b350aaca34ac1
moroha10085 <50521431+moroha10085@users.noreply.github.com>
2022-08-23 14:33:35 +0900

    Update script.js

--- stars/script.js
+++ stars/script.js
@@ -120,8 +120,5 @@ function draw() {
 }
 
 function mousePressed() {
-  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-    let fs = fullscreen();
-    fullscreen(!fs);
-  }
+    fullscreen(!fullscreen());
 }
